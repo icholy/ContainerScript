4576d599902e69a89d751154760ecd990f3d7657
Ilia Choly <ilia.choly@gmail.com>
2024-11-28 22:07:53 -0500

    disable chunking

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].bundle.js'
+		filename: '[name].bundle.js',
+		chunkFormat: false
 	},
 	module: {
 		rules: [
